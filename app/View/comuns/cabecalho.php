5fc0fa2ac995e69e7a30f2c5ba9d39bf419f5759
Thiago Nunes <thiagolimanunes@hotmail.com>
2025-06-22 20:21:07 -0300

    Modifica o nome do usuário logado no communs

--- app/View/comuns/cabecalho.php
+++ app/View/comuns/cabecalho.php
@@ -56,7 +56,7 @@ use Core\Library\Session;
 
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Usuário
+                                    <?= htmlspecialchars(Session::get("userNome") ?? 'Usuário') ?>
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="<?= baseUrl() ?>login/signOut">Sair</a></li>
